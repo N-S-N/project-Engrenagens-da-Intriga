37aaba81b746b0efa36744148eebef97acd058b9
N-S-N <135265220+N-S-N@users.noreply.github.com>
2024-01-31 18:52:38 -0300

    sistema

---
+++ Engrenagens da Intriga/Assets/EngrenagemControler.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class EngrenagemControler : MonoBehaviour
+{
+    bool acertou = false;
+
+    [Header("Scripts")]
+    [SerializeField] UiEnterection Brem;
+    [SerializeField] Slider Slider;
+
+    [Header("Resposta")]
+    [SerializeField] int _gabartito;
+
+    [Header("OBJ")]
+    [SerializeField] GameObject engrenagem;
+    [SerializeField] GameObject SliderFolt;
+    [SerializeField] GameObject ButomFolt;
+
+
+    PlayerInput playerInput;
+
+    void OnEnable()
+    {
+        playerInput = Brem.player.GetComponent<PlayerInput>();
+    }
+    void Update()
+    {
+        //sair
+        if (playerInput.actions["Quit"].triggered)
+        {
+            ButomFolt.SetActive(true);
+            SliderFolt.SetActive(false);
+        }
+
+        //sistema de rotação
+        if (Slider.value == 9) Slider.value = 1;
+        if (Slider.value == 0) Slider.value = 8;
+
+        //sistema de rotação
+        engrenagem.transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotencionFuncion()));
+
+        //sistema de ferificação
+        //tava certo e saio
+        if (Slider.value != _gabartito && acertou)
+        {
+            Brem.CauntCorretsInfo--;
+            acertou = false;
+        }
+        //tava errado e acertou
+        if (Slider.value == _gabartito && !acertou)
+        {
+            Brem.CauntCorretsInfo++;
+            acertou = true;
+        }
+
+    }
+    
+    float rotencionFuncion()
+    {
+        float numeroRotencion = 45; //360 / 8 que e a contidade de totação deste objeto
+
+        float rotecionAtual = numeroRotencion * Slider.value-1; // a rotação que esta atuando neste momento
+
+        return rotecionAtual;
+    }
+
+}
--- Engrenagens da Intriga/Assets/Script/UiEnterection.cs
+++ Engrenagens da Intriga/Assets/Script/UiEnterection.cs
@@ -1,22 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem.UI;
 
 public class UiEnterection : MonoBehaviour
 {
     Archibald playerScript;
     enterectionScript PaiScriopt;
-    [SerializeField] Canvas canva;
+
+    [Header("Canvas Ui")]
+    [SerializeField] Canvas canva; //canva onde tem o enigma
+
+    [Header("quantos problema tem que ser resolvido")]
+    [SerializeField] int _Number;
+
+    [Header("Quantidade de problema resovido")]
+    public int CauntCorretsInfo = 0; //Quantidade de problema resovido
+
+    [Header("Envents Sistem")]
+    [SerializeField] MultiplayerEventSystem[] EventSystems;
+
+    public GameObject player;
+    //start
 
     private void OnEnable()
     {
         PaiScriopt = GetComponentInParent<enterectionScript>();
         playerScript = PaiScriopt.PlayerObj.GetComponent<Archibald>();
         canva.worldCamera = playerScript.mainCamera;
+        player = PaiScriopt.PlayerObj;
+        for (int i = 0; i < EventSystems.Length; i++)
+        {
+            EventSystems[i].playerRoot = PaiScriopt.PlayerObj;
+        }
     }
 
+    //atolizaçõas
+    private void FixedUpdate()
+    {
+        if (_Number == CauntCorretsInfo)
+        {
+            EnigmaResolucion();
+        }
+    }
+
+    //relução
     public void EnigmaResolucion()
     {
         playerScript.resoveu = true;
         Destroy(PaiScriopt.gameObject);
-
     }
+
 }
